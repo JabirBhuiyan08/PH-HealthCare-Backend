a4234ebc750b97a3be46d10c123be3a3a62834eb
Jabir Bhuiyan <jabirbhuiyan08@gmail.com>
2026-02-13 09:35:28 +0600

    first commit

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { SpecialtyRoutes } from "../module/specialty.route";
+import { SpecialtyRoutes } from "../module/specialty/specialty.route";
+import { AuthRoutes } from "../module/auth/auth.router";
 
 const router = Router()
 
+router.use("/auth", AuthRoutes)
 router.use("/specialties", SpecialtyRoutes)
 
 export const IndexRoutes = router
